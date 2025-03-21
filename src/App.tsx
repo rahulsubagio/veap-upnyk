8708ea9d458c6658059a7856e80a596f3850e5d1
rahulsubagio <rahulsubagio99@gmail.com>
2025-03-21 14:56:37 +0700

    update hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import logoGreenPyramid from './assets/logo-gp-n.png'
-import photoGreenPyramid from './assets/green-house-pyramid.jpg'
+// import photoGreenPyramid from './assets/green-house-pyramid.jpg'
+import bgGreenPyramid from './assets/green-house-pyramid-removebg-preview-1.png'
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,7 +12,7 @@ function App() {
       <div className="bg-white">
         {/* Navbar */}
         <div className='relative'>
-          <nav className="fixed top-0 w-full flex justify-between items-center p-4 shadow-md bg-bgjo">
+          <nav className="fixed top-0 w-full p-4 flex justify-between items-center shadow-sm bg-white">
             {/* logo */}
             <a href='#' className='flex items-center'>
               <img src={logoGreenPyramid} alt="logo green pyramid" className='mr-1 h-8' />
@@ -36,7 +37,7 @@ function App() {
               </li>
             </ul>
             <button className='hidden md:block'>
-              <a href="#dashboardIoT" className='bg-toska text-white font-semibold py-2 px-3 rounded-md hover:bg-teal-500'>Dashboard IoT <span className="icon-[material-symbols--call-made-rounded]"></span></a>
+              <a href="#dashboardIoT" className='bg-toska text-white font-semibold py-2 px-3 rounded-lg hover:bg-teal-500'>Dashboard IoT <span className="icon-[material-symbols--call-made-rounded]"></span></a>
             </button>
             {/* menu button */}
             <button
@@ -53,7 +54,7 @@ function App() {
 
           {/* menu option */}
           {isOpen && (
-            <div className='fixed top-16 w-full px-4 pb-4 shadow-md bg-bgjo md:hidden'>
+            <div className='fixed top-16 w-full px-4 pb-4 shadow-md bg-white md:hidden'>
               <ul className="pt-4 pb-2 px-6 font-semibold">
                 <li className='border-b border-gray-300 mb-1 pb-1'>
                   <a href="#home" className="block py-2 hover:text-toska">Home</a>
@@ -68,7 +69,7 @@ function App() {
                   <a href="#aboutUs" className="block py-2 hover:text-toska">About Us</a>
                 </li>
                 <li className='mt-6 text-center'>
-                  <a href="#dashboardIoT" className='block py-2 px-4 rounded-lg outline-1 text-toska font-bold hover:bg-toska hover:text-white hover:outline-toska'>Dashboard IoT <span className="icon-[material-symbols--call-made-rounded]"></span></a>
+                  <a href="#dashboardIoT" className='block py-2 px-4 rounded-lg outline-1 text-toska font-semibold hover:bg-toska hover:text-white hover:outline-toska'>Dashboard IoT <span className="icon-[material-symbols--call-made-rounded]"></span></a>
                 </li>
               </ul>
             </div>
@@ -77,18 +78,21 @@ function App() {
         {/* Navbar end */}
 
         {/* Hero section */}
-        <section className='pt-22 px-6 flex flex-col justify-between bg-bgjo h-dvh'>
-          <div className=''>
-            <img src={photoGreenPyramid} alt="green pyramid photo" className='rounded-2xl -outline-offset-10 outline-3 outline-bgjo' />
-          </div>
-          <div className='text-center'>
-            <h1 className='text-3xl font-bold'>Welcome to Green Pyramid</h1>
+        <section className='flex flex-col justify-between items-center h-dvh bg-white'>
+          <div className='pt-22 px-6 flex flex-col items-center text-center'>
+            <h1 className='mt-2 text-3xl font-bold'>Welcome to Green Pyramid</h1>
             <p className='mt-2 text-sm'>
               Green Pyramid is a modern agricultural facility that supports research and learning while implementing IoT technology to enhance crop cultivation efficiency.
             </p>
+            <a href="#plantCare" className='mt-2 py-2 px-8 rounded-lg bg-toska text-white font-semibold hover:bg-teal-500'>Plant Care</a>
           </div>
+          <img src={bgGreenPyramid} alt="green pyramid photo" className='w-full bg-bottom' />
         </section>
         {/* Hero section end */}
+
+        <section className='bg-bgjo h-dvh'>
+          <div><h1 className='text-9xl'>haloo</h1></div>
+        </section>
       </div>
     </>
   )
